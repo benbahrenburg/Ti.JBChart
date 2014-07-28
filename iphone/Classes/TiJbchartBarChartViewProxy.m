82249e7451abccaeb2b086fdf0baa3d23b1be734
Ben Bahrenburg <ben.bahrenburg@gmail.com>
2014-07-28 08:50:16 -0700

    add initial bar chart proxies

--- iphone/Classes/TiJbchartBarChartViewProxy.m
+++ iphone/Classes/TiJbchartBarChartViewProxy.m
@@ -1,8 +1,12 @@
 /**
- * Appcelerator Titanium Mobile
- * Copyright (c) 2009-2014 by Appcelerator, Inc. All Rights Reserved.
- * Licensed under the terms of the Apache Public License
+ * Ti.JBChart (https://github.com/benbahrenburg/Ti.JBChart)
+ *
+ * Titanium code Copyright (c) 2009-2014 by Ben Bahrenburg. All Rights Reserved.
+ * Licensed under the terms of the Apache 2.0 License
  * Please see the LICENSE included with this distribution for details.
+ *
+ * Ti.JBChart is a Titanium wrapper for JBChartView
+ * for more information please visit https://github.com/Jawbone/JBChartView
  */
 
 #import "TiJbchartBarChartViewProxy.h"
